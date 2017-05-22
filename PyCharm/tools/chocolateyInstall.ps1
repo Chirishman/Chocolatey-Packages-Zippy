e6d73b442e361cd39b01a7efd8871ab1a0d7cc6d
Chirishman <Chirishman@users.noreply.github.com>
2017-05-22 13:08:58 -0400

    Update chocolateyInstall.ps1
    bumped to current version (2017.1.2)

--- PyCharm/tools/chocolateyInstall.ps1
+++ PyCharm/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'Pycharm'
 $installerType = 'EXE'
-$url = 'http://download.jetbrains.com/python/pycharm-professional-4.5.1.exe'
+$url = 'https://download-cf.jetbrains.com/python/pycharm-professional-2017.1.2.exe'
 $url64 = $url
 $silentArgs = '/S'
 $validExitCodes = @(0)
